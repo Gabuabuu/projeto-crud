166107b5a37e149a9e70066ab2a898d5c8c25f38
Gabuabuu <gabydavilaneves@gmail.com>
2023-03-26 21:10:28 -0300

    feat: :sparkles: CRUD: Iniciando "base de dados"

---
+++ crud.js
@@ -0,0 +1,16 @@
+// Base de Dados Nossa
+
+const miniTwitter = {
+    usuarios: [
+        {
+            username: 'gabryel'
+        }
+    ],
+    posts: [
+        {
+            id: 1,
+            owner: 'gabryel',
+            content: 'Meu primeiro post'
+        }
+    ]
+}
