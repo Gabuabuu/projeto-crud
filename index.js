ee43a5fcd5718d54c8b3a313e1a7b7100285afd3
Gabuabuu <gabydavilaneves@gmail.com>
2023-03-26 21:58:45 -0300

    refactor: :recycle:

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Base de Dados Nossa
+// "Modelo" // Guarda dados da aplicação
 
 const miniRedeSocial = {
     usuarios: [
@@ -13,13 +13,15 @@ const miniRedeSocial = {
             content: 'Meu primeiro post'
         } // Sempre que for criado um "post" novo, esse array (posts) será atualizado
     ],
-    criaPost(dados) {
-        // Cria Posts na memoria (Array/Objeto)
-        miniRedeSocial.posts.push({
-            id: miniRedeSocial.posts.length + 1,
-            owner: dados.owner,
-            content: dados.content
-        });
+    criaPost(dados, htmlOnly = false) {
+        if (!htmlOnly) {
+            // Cria Posts na memoria (Array/Objeto)
+            miniRedeSocial.posts.push({
+                id: miniRedeSocial.posts.length + 1,
+                owner: dados.owner,
+                content: dados.content
+            });
+        }
         // Cria post no html
         const $listaDePosts = document.querySelector('.listaDePosts');
         $listaDePosts.insertAdjacentHTML('afterbegin', `<li>${dados.content}</li>`)
@@ -31,10 +33,18 @@ const miniRedeSocial = {
 const $meuForm = document.querySelector('form'); // $ para saber que a variavel está guardando um elemento do html
 console.log($meuForm);
 
+// CRUD: [READ]
+miniRedeSocial.posts.forEach(({ owner, content }) => {
+    miniRedeSocial.criaPost({ owner: owner, content: content }, true)
+
+})
+
+
+// CRUD: [CREATE]
 $meuForm.addEventListener('submit', function criaPostController(infosDoEvento) {
     infosDoEvento.preventDefault()
     const $campoCriaPost = document.querySelector('input[name="criaPost"]');
-    miniRedeSocial.criaPost({ owner: 'gabryel', content: 'segundo post' })
+    miniRedeSocial.criaPost({ owner: 'gabryel', content: $campoCriaPost.value })
     console.log(miniRedeSocial.posts)
 
 
